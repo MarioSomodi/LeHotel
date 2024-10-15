0b682ed280a8cb93fd0a71b045a5ca13ec7e479e
MarioSomodi <mario0.0somodi@gmail.com>
2024-10-15 20:34:46 +0200

    feat: Add base models 🧱

---
+++ LeHotel.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,18 @@
+﻿namespace LeHotel.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+            where TId : notnull
+    {
+        protected AggregateRoot(TId id)
+            : base(id)
+        { }
+        /// <summary>
+        /// Needed for instancing in ef core
+        /// </summary>
+#pragma warning disable CS8618
+        protected AggregateRoot()
+        {
+        }
+#pragma warning restore CS8618 
+    }
+}
