2e1d126d40b4763fdbdb4c5606e6a263b1e70797
msomodi <mario0.0somodi@gmail.com>
2024-10-16 11:47:46 +0200

    chore: configure FluentValidation pipeline ⚙️

--- LeHotel.Application/DependencyInjection.cs
+++ LeHotel.Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LeHotel.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
 
 namespace LeHotel.Application
 {
@@ -7,6 +11,11 @@ namespace LeHotel.Application
         public static IServiceCollection AddAplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             return services;
         }
