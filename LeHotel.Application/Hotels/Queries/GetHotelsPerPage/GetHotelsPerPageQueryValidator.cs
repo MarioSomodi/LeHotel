67bd7fbb065b3749c5145d4c18ca60e7e505d66f
msomodi <mario0.0somodi@gmail.com>
2024-10-16 11:52:38 +0200

    feat: Add validation to GetHotelsPerPageQuery ✅

---
+++ LeHotel.Application/Hotels/Queries/GetHotelsPerPage/GetHotelsPerPageQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace LeHotel.Application.Hotels.Queries.GetHotelsPerPage
+{
+    public class GetHotelsPerPageQueryValidator : AbstractValidator<GetHotelsPerPageQuery>
+    {
+        public GetHotelsPerPageQueryValidator()
+        {
+            RuleFor(gHPP => gHPP.Page).NotEmpty().GreaterThan(0);
+            RuleFor(cPC => cPC.PageSize).NotEmpty().GreaterThan(0);
+        }
+    }
+}
